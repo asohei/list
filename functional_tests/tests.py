d652f63a709c3a6590491013efcb7088cd6b22db
asohei <ns.cebu@gmail.com>
2014-10-23 07:09:32 +0900

    make functional_tests an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,9 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
+#import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -21,7 +22,7 @@ class NewVisitorTest(unittest.TestCase):
 
 		#Edith heard about the cool online service. She goes out to check its homepage
 
-		self.browser.get('http://localhost:8000')
+		self.browser.get(self.live_server_url)
 		
 		
 		#she notices the page title and header mention todo
@@ -78,8 +79,5 @@ class NewVisitorTest(unittest.TestCase):
 # Satisfied, she goes back to sleep
 
 
-
-if __name__ == '__main__':
-	unittest.main(warnings='ignore')
 
 
