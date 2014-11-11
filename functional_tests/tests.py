2ebbbb61c1f331e90f358ba75fabda274f67157e
asohei <ns.cebu@gmail.com>
2014-11-12 06:43:55 +0900

    add a url for creating a new list via POST

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -61,9 +61,7 @@ class NewVisitorTest(LiveServerTestCase):
 
 
 # The page updates again, and now shows both items on her list
-	
-		print (self.browser.current_url)
-		
+
 	
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
