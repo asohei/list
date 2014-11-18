88f894a9a4814c251f2995d0709a2a7ba70d874c
asohei <ns.cebu@gmail.com>
2014-11-19 06:20:42 +0900

    Hack FT runner to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,24 @@
+import sys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 #import unittest
 
 class NewVisitorTest(StaticLiveServerTestCase):
+
+	@classmethod
+	def setUpClass(cls):
+		for arg in sys.argv:
+			if 'liveserver' in arg:
+				cls.server_url = 'http://' + arg.split('=')[1]
+				return
+		super().setUpClass()
+		cls.server_url = cls.live_server_url
+		
+	@classmethod
+	def tearDownClass(cls):
+		if cls.server_url == cls.live_server_url:
+			super().tearDownClass()
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -21,7 +36,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 	def test_layout_and_styling(self):
 	
 		#Edith goes to the home page
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		self.browser.set_window_size(1024, 768)
 		
 		#She notices the input box is nicely centered
@@ -40,7 +55,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		#Edith heard about the cool online service. She goes out to check its homepage
 
 		
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		
 		
 		#she notices the page title and header mention todo
@@ -91,7 +106,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		
 #Francis visits the web site. There is no sign of Edith's list
 
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock  feathers', page_text)
