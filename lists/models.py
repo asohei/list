ea4bc7bda648d07380dbf0ee99d7253542d988d0
sohei <ns.cebu@gmail.com>
2015-01-07 07:05:35 +0900

    duplication test added to the test@ @

--- lists/models.py
+++ lists/models.py
@@ -2,16 +2,21 @@ from django.db import models
 from django.core.urlresolvers import reverse
 
 class List(models.Model):
-	
-	def get_absolute_url(self):
-		return reverse('view_list', args=[self.id])
-	
-	pass
+    
+    def get_absolute_url(self):
+        return reverse('view_list', args=[self.id])
+        pass
 
 class Item(models.Model):
-	
-	text = models.TextField(default = '')
-	list = models.ForeignKey(List, default =None)
-
-	pass
+    text = models.TextField(default = '')
+    list = models.ForeignKey(List, default =None)
+    
+    class Meta:
+        ordering = ('id',)
+        unique_together = ('list', 'text')
+    
+    def __str__(self):
+        return self.text
+    
+    pass
 
