feaf9c00bf34c9fe405256220b350931ed01d0be
sohei <ns.cebu@gmail.com>
2015-01-30 06:11:07 +0900

    persona backend and user model

--- superlists/settings.py
+++ superlists/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'lists',
+    'lists',
+    'accounts',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -84,3 +85,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '../static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'superlists', 'static'),)
+
+AUTH_USER_MODEL = 'accounts.User'
+AUTHENTICATION_BACKENDS = ('accounts.authentication.PersonaAuthenticationBackend')
